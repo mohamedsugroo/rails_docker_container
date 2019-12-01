1071e41d03f9c69ac7d9ef58748e4c5cd01f8d2b
mohamedsugroo <mohamedsugroo@gmail.com>
2019-12-01 12:13:21 +0000

    time fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    render json: "server ready to go #{Time.now}"
+    render json: "server ready to go !!!! 1234 #{Time.now}"
   end
 
 end
